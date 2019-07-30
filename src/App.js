27424c02438ac6796fb0cbf80b62c008b13bc6df
Suphapan Boonngoen [TH] <SBoonngoen@kerrylogistics.com>
2019-07-30 10:38:55 +0700

    ใช้ ant design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HeaderBar from './components/HeaderBar';
+
+import { Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Layout className="layout">
+        <HeaderBar />
+        <Content style={{ padding: '0 50px' }}>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
     </div>
   );
 }
---
+++ src/components/HeaderBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Layout, Menu } from 'antd';
+const { Header } = Layout;
+
+export default class HeaderBar extends Component {
+    render() {
+        return (
+            <Header>
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['1']}>
+                    <Menu.Item key="1">Dashboard</Menu.Item>
+                </Menu>
+            </Header>
+        )
+    }
+}
