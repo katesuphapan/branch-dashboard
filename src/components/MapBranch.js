30192906650431e91cfc561e31851e9a9099dd6b
Suphapan Boonngoen [TH] <SBoonngoen@kerrylogistics.com>
2019-07-30 11:21:42 +0700

    create map and marker

---
+++ src/components/MapBranch.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import GoogleMapReact from "google-map-react"
+
+export default class MapBranch extends Component {
+
+    static defaultProps = {
+        // Kerry Siam Seaport Location
+        center: {
+            lat: 13.7200452,
+            lng: 100.5135078
+        },
+        zoom: 15
+    };
+
+    handleApiLoaded(map, maps) { 
+
+        let marker = new maps.Marker({
+            position: this.props.center,
+            map,
+            title: 'Kerry'
+        });
+    }
+
+    render() {
+        return (
+            <div style={
+                {
+                    height: '100vh',
+                    width: '100%'
+                }
+
+            }>
+                <GoogleMapReact
+                    bootstrapURLKeys={{
+                        key: 'AIzaSyBDqlW1EIlePcA48oLVV_kYQJXm9dQ75uw'
+                    }}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                    yesIWantToUseGoogleMapApiInternals
+                    onGoogleApiLoaded={({ map, maps }) => this.handleApiLoaded(map, maps)}
+                >
+                    
+                    
+
+                </GoogleMapReact>
+            </div>
+        )
+    }
+}
